15d4428c4df141a936275e0b13e629f88f9d9934
larislav <laarissa_barros@hotmail.com>
2022-08-27 20:33:24 -0300

    Acesso a dados API Catalogo

---
+++ src/building blocks/NSE.Core/Data/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NSE.Core.DomainObjects;
+
+namespace NSE.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+        //Apenas 1 repositório por agregação
+
+    }
+}
