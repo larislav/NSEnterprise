e9f727d24addd7cf37279520712c8c6dad9002c7
larislav <laarissa_barros@hotmail.com>
2022-09-12 23:21:45 -0300

    Definindo entidades e validações

---
+++ src/building blocks/NSE.Core/DomainObjects/Email.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NSE.Core.DomainObjects
+{
+    public class Email
+    {
+        public const int EnderecoMaxLength = 254;
+        public const int EnderecoMinLength = 5;
+        public string Endereco { get; private set; }
+
+        public Email()
+        {
+        }
+
+        public Email(string endereco)
+        {
+           
+            Endereco = endereco;
+        }
+
+        public static bool Validar(string email)
+        {
+            var regexEmail = new Regex(@"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
+            return regexEmail.IsMatch(email);
+        }
+    }
+}
